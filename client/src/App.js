9dba37408d53dec89f5c3ee382d9381c8e2ba2bb
tmcash <t.cash15@yahoo.com>
2023-05-14 19:49:17 -0400

    Trying to fix things to deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import {ApolloClient, ApolloProvider,InMemoryCache,createHttpLink,} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
