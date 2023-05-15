f9559b7683bde77d8088883355935b9a465595b8
tmcash <t.cash15@yahoo.com>
2023-05-15 19:32:24 -0400

    Fixed some code

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
